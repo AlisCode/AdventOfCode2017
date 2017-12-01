7326a81f1cd4f9544f606359c3a3f89b0e86a539
AlisCode <oliv.pinon@gmail.com>
2017-12-01 13:20:33 +0100

    Second half of day1 done + changed crate name

--- src/day1.rs
+++ src/day1.rs
@@ -5,3 +5,11 @@ pub fn decode_captcha_iter(captcha: &str) -> u32 {
         .filter_map(|(a, b)| if a == b { a.to_digit(10) } else { None })
         .sum()
 }
+
+pub fn decode_captcha_iter_second(captcha: &str) -> u32 {
+    captcha
+        .chars()
+        .zip(captcha.chars().cycle().skip(captcha.len() / 2))
+        .filter_map(|(a, b)| if a == b { a.to_digit(10) } else { None })
+        .sum()
+}
--- tests/day1.rs
+++ tests/day1.rs
@@ -1,6 +1,7 @@
-extern crate AdventOfCode2017;
+extern crate advent_of_code_2017;
 
-use AdventOfCode2017::day1::decode_captcha_iter;
+use advent_of_code_2017::day1::decode_captcha_iter;
+use advent_of_code_2017::day1::decode_captcha_iter_second;
 
 #[test]
 fn test_day1_first_iter() {
@@ -8,4 +9,13 @@ fn test_day1_first_iter() {
     assert_eq!(decode_captcha_iter("1111"), 4);
     assert_eq!(decode_captcha_iter("1234"), 0);
     assert_eq!(decode_captcha_iter("91212129"), 9);
+}
+
+#[test]
+fn test_day1_second_iter() {
+    assert_eq!(decode_captcha_iter_second("1212"), 6);
+    assert_eq!(decode_captcha_iter_second("1221"), 0);
+    assert_eq!(decode_captcha_iter_second("123425"), 4);
+    assert_eq!(decode_captcha_iter_second("123123"), 12);
+    assert_eq!(decode_captcha_iter_second("12131415"), 4);
 }
