4b519061a6365dd360d22ade04ce3c3056339186
AlisCode <oliv.pinon@gmail.com>
2017-12-02 19:09:51 +0100

    more idiomatic solution

--- src/day2.rs
+++ src/day2.rs
@@ -1,57 +1,60 @@
+/// Resolves the first part of the problem
 pub fn decode_spreadsheet(sheet: &str) -> u32 {
     sheet
-        .split("\n")
-        .map(|a| {
-            a.split_whitespace()
-                .map(|b| b.to_string().parse::<u32>().unwrap())
-                .max()
-                .unwrap() -
-                a.split_whitespace()
-                    .map(|b| b.to_string().parse::<u32>().unwrap())
-                    .min()
-                    .unwrap()
-        })
+        .lines()
+        .filter_map(find_min_max)
+        .map(|(min, max)| max-min)
         .sum()
 }
 
-fn find_evenly_divisible_values(list: Vec<u32>) -> (u32, u32)
-{
-    let todo: Vec<u32> = list.clone();
-    let mut return_tuple: (u32,u32) = (1,1);
-    todo.into_iter().for_each(|a| {
-        let elem: u32 = list.iter().cloned().filter(|b| are_evenly_divisible(a,*b)).last().unwrap_or(0);
-        if elem != 0 { return_tuple = ordered_tuple(a, elem); return; }
-    });
-
-    return_tuple
-}
-
-fn are_evenly_divisible(a: u32, b: u32) -> bool
-{
-    if a > b { return a % b == 0 }
-    else if a < b { return b % a == 0 }
-    false
-}
-
-fn ordered_tuple(a: u32, b: u32) -> (u32, u32)
-{
-    if a > b { return (a,b); }
-    return (b,a);
-}
-
+/// Resolves the second part of the problem
 pub fn decode_spreadsheet_second(sheet: &str) -> u32 {
-
     sheet
-        .split("\n")
-        .map(|a| {
-
-            let (b, c) = find_evenly_divisible_values(
-                a.split_whitespace()
-                    .map(|b| b.to_string().parse::<u32>().unwrap())
-                    .collect()
-            );
-
-            b / c
-        })
+        .lines()
+        .filter_map(find_evenly_divisible_numbers)
+        .map(|(a,b)| a / b)
         .sum()
+}
+
+/// Finds the minimum and maximum of a list given as white-spaced u32-containable numbers 
+/// returns a tuple containing (min, max)
+pub fn find_min_max(line: &str) -> Option<(u32, u32)>
+{
+    line
+        .split_whitespace()
+        .filter_map(|a| a.parse::<u32>().ok())
+        .fold(
+            None,
+            |min_max, number| if let Some((min, max)) = min_max {
+                Some((u32::min(min, number), u32::max(max, number)))
+            } else {
+                Some((number, number))
+        })
+}
+
+/// Finds the two only evenly divisible numbers of a list give as white-spaced u32-containable numbers
+/// returns a tuple containing (a,b) the two evenly divisible numbers of the list, or None if we didn't find any
+pub fn find_evenly_divisible_numbers(line: &str) -> Option<(u32, u32)> {
+    
+    // Gets the list as u32 numbers
+    // NB : the list is mutable because we need to sort it
+    let mut list: Vec<u32> = 
+    line
+    .split_whitespace()
+    .filter_map(|a| a.parse::<u32>().ok())
+    .collect();
+
+    // Sorts the list so we have numbers in ascending order (i.e here, number a evenly divides number b)
+    list.sort();
+
+    // Iterates over the rest of the list, searching for the number that would evenly divide the number a that we're observing
+    for (i, &a) in list.iter().enumerate() {
+        for &b in list[i + 1..].iter() {
+            // Found it! Returning the tuple ...
+            if b % a == 0 { return Some((b,a)); }
+        }
+    }
+
+    // In case we haven't found anything, though it is impossible in this case theoretically... 
+    None
 }
--- tests/day2.rs
+++ tests/day2.rs
@@ -2,10 +2,21 @@ extern crate advent_of_code_2017;
 
 use advent_of_code_2017::day2::decode_spreadsheet;
 use advent_of_code_2017::day2::decode_spreadsheet_second;
+use advent_of_code_2017::day2::find_min_max;
+use advent_of_code_2017::day2::find_evenly_divisible_numbers;
 
-// Sample case for part 1
+// One-line-only sample case for part 1
 #[test]
-fn test_decode_sample1() {
+fn day2_test_decode_sample1() {
+
+    let input = "5 1 9 5";
+    assert_eq!(find_min_max(input), Some((1,9)));
+
+}
+
+// Complete sample case for part 1
+#[test]
+fn day2_test_decode_sample2() {
 
     let input = "5 1 9 5
     7 5 3
@@ -16,8 +27,9 @@ fn test_decode_sample1() {
 
 
 // This was unexpected, but numbers can have more than one digit lol
+// So here is a test
 #[test]
-fn test_decode_sample2() {
+fn day2_test_decode_sample3() {
 
     let input = "51 13 94 53
     72 54 35
@@ -26,9 +38,20 @@ fn test_decode_sample2() {
 
 }
 
-// Sample case for part 2
+
+// One-line-only sample case for part 2
 #[test]
-fn test_decode_second_sample1() {
+fn day2_test_decode_second_sample1() {
+
+    let input = "5 9 2 8";
+    assert_eq!(find_evenly_divisible_numbers(input), Some((8,2)));
+
+}
+
+
+// Complete sample case for part 2
+#[test]
+fn day2_test_decode_second_sample2() {
 
     let input = "5 9 2 8
     9 4 7 3
