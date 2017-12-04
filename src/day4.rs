8d8cd6ee48a9a1ae0b4b8495c393538b63f2430f
Olivier <olivier@localhost.localdomain>
2017-12-04 20:47:59 +0100

    fixed hash mapping of words :)

--- src/day4.rs
+++ src/day4.rs
@@ -1,59 +1,79 @@
 pub fn is_passphrase_ok_first(input: &str) -> bool {
-    
-	// Get all the words as a list
-	let mut list: Vec<&str> =
-		input
-		.split_whitespace()
-		.collect();
-		
-	// Get the length of the list
-	let len: usize = list.len();
 
-	// Sorts, then removes duplicates within the list
-	list.sort();
-	list.dedup();
-	    
-	// Compares the new length of the list with the previous one.
-	// The idea here is that if the length isn't the same as before, it means that we removed a duplicated word,
-	// so the passphrase isn't valid.
-	len == list.len()
+    // Get all the words as a list
+    let mut list: Vec<&str> = input.split_whitespace().collect();
+
+    // Get the length of the list
+    let len: usize = list.len();
+
+    // Sorts, then removes duplicates within the list
+    list.sort();
+    list.dedup();
+
+    // Compares the new length of the list with the previous one.
+    // The idea here is that if the length isn't the same as before, it means that we removed a duplicated word,
+    // so the passphrase isn't valid.
+    len == list.len()
 }
 
 /// Second part
 pub fn is_passphrase_ok_second(input: &str) -> bool {
-    
-	// Gets all word's hashes in a list
-	let mut list: Vec<i32> =
-    input
-    	.split_whitespace()
-    	.map(|a| prime_sum(a))
-    	.collect();
-	
-	// Same as part 1 :)
+
+    // Gets all word's hashes in a list
+    let mut list: Vec<i64> = input.split_whitespace().map(|a| prime_hash(a)).collect();
+
+    // Same as part 1 :)
     let len: usize = list.len();
-    
-	list.sort();
+
+    list.sort();
     list.dedup();
-    
-	// Return the passphrase's "validity"
+
+    // Return the passphrase's "validity"
     len == list.len()
 }
 
 /// Calculates the prime hash for a given word
-pub fn prime_sum(input: &str) -> i32 {
+pub fn prime_hash(input: &str) -> i64 {
 
-	// Stores the 26 first primes so we can map each char to its prime number
-    let primes = vec!(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101);
+    // Stores the 26 first primes so we can map each char to its prime number
+    let primes: Vec<i64> = vec![
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47,
+        53,
+        59,
+        61,
+        67,
+        71,
+        73,
+        79,
+        83,
+        89,
+        97,
+        101,
+    ];
 
-	// For each char, multiply its ASCII value (char as u8 or u16 in this case) 
-	// by the corresponding prime in the primes array 
-    // then sum all the chars up, we then get the hash of the word's letters 
-	input
-	    .chars()
-	    .map(|a| (((a as u16) * primes[((a as u16) - 97) as usize])) as i32)
-	    .sum()
+    // For each char, multiply its ASCII value (char as i64 to avoid overflow in this case)
+    // by the corresponding prime in the primes array
+    // then sum all the chars up, we then get the hash of the word's letters
+    input
+        .chars()
+        .map(|a| primes[((a as i64) - 97) as usize] as i64)
+        .product()
 
-	// A word's hash is by nature unique, because of the multiplication with prime numbers. 
-	// this means that if two words have the same hash, they can be formed with the same letters
-	// i.e. : they are anagrams.
+    // A word's hash is by nature unique, because of the multiplication with prime numbers.
+    // this means that if two words have the same hash, they can be formed with the same letters
+    // i.e. : they are anagrams :)
 }
--- tests/day4.rs
+++ tests/day4.rs
@@ -2,56 +2,56 @@ extern crate advent_of_code_2017;
 
 use advent_of_code_2017::day4::is_passphrase_ok_first;
 use advent_of_code_2017::day4::is_passphrase_ok_second;
-use advent_of_code_2017::day4::prime_sum;
+use advent_of_code_2017::day4::prime_hash;
 
 #[test]
 fn test_day4_passphrase_first_sample1() {
-	assert_eq!(is_passphrase_ok_first("aa bb cc dd ee"), true);
+    assert_eq!(is_passphrase_ok_first("aa bb cc dd ee"), true);
 }
 
 #[test]
 fn test_day4_passphrase_first_sample2() {
-	assert_eq!(is_passphrase_ok_first("aa bb cc dd aa"), false);
+    assert_eq!(is_passphrase_ok_first("aa bb cc dd aa"), false);
 }
 
 #[test]
 fn test_day4_passphrase_first_sample3() {
-	assert_eq!(is_passphrase_ok_first("aa bb cc dd aaa"), true);
+    assert_eq!(is_passphrase_ok_first("aa bb cc dd aaa"), true);
 }
 
 #[test]
 fn test_day4_anagram_sample1() {
-	let val: bool = prime_sum("test") == prime_sum("estt");
-	assert_eq!(val, true);
+    let val: bool = prime_hash("test") == prime_hash("estt");
+    assert_eq!(val, true);
 }
 
 #[test]
 fn test_day4_anagram_sample2() {
-	let val: bool = prime_sum("west") == prime_sum("rstt");
-	assert_eq!(val, false);
+    let val: bool = prime_hash("west") == prime_hash("rstt");
+    assert_eq!(val, false);
 }
 
 #[test]
 fn test_day4_passphrase_second_sample1() {
-	assert_eq!(is_passphrase_ok_second("abcde fghij"), true);
+    assert_eq!(is_passphrase_ok_second("abcde fghij"), true);
 }
 
 #[test]
 fn test_day4_passphrase_second_sample2() {
-	assert_eq!(is_passphrase_ok_second("abcde xyz ecdab"), false);
+    assert_eq!(is_passphrase_ok_second("abcde xyz ecdab"), false);
 }
 
 #[test]
 fn test_day4_passphrase_second_sample3() {
-	assert_eq!(is_passphrase_ok_second("a ab abc abd abf abj"), true);
+    assert_eq!(is_passphrase_ok_second("a ab abc abd abf abj"), true);
 }
 
 #[test]
 fn test_day4_passphrase_second_sample4() {
-	assert_eq!(is_passphrase_ok_second("iiii oiii ooii oooi oooo"), true);
+    assert_eq!(is_passphrase_ok_second("iiii oiii ooii oooi oooo"), true);
 }
 
 #[test]
 fn test_day4_passphrase_second_sample5() {
-	assert_eq!(is_passphrase_ok_second("oiii ioii iioi iiio"), false);
+    assert_eq!(is_passphrase_ok_second("oiii ioii iioi iiio"), false);
 }
