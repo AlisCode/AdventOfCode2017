4be42eb80f432358f4b687aae8091896b4f7f76a
Olivier <olivier@localhost.localdomain>
2017-12-10 16:16:36 +0100

    Day 10 in progres

--- src/day7.rs
+++ src/day7.rs
@@ -8,15 +8,10 @@ pub struct Process<'a> {
 
 impl<'a> Process<'a> {
     pub fn new(input: &'a str) -> Self {
-        let mut iter_input = input.split_whitespace();
-        let name: &str = iter_input.next().unwrap();
-        let weight: i32 = iter_input.next().unwrap().parse::<i32>().unwrap();
-
-        println!("got a new process : name {}, weight: {}", name, weight);
 
         Process {
-            name,
-            weight,
+            name: "test",
+            weight: 1,
             children_names: Vec::new(),
             children_process: Vec::new(),
             parent: None,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 extern crate advent_of_code_2017;
 
-fn main() {}
+use advent_of_code_2017::day10::KnotHasher;
+
+fn main() {
+
+    let mut my_knot_hasher: KnotHasher = KnotHasher::new();
+    let input: &str = "199,0,255,136,174,254,227,16,51,85,1,2,22,17,7,192";
+
+    my_knot_hasher.do_knot_for_input_first(input);
+    println!("res: {}", my_knot_hasher.get_result());
+}
