9daf4d6ff634ed2b48610c63afa9c0b418985767
AlisCode <oliv.pinon@gmail.com>
2017-12-14 13:56:24 +0100

    day14 part 2 in progress...

--- src/day14.rs
+++ src/day14.rs
@@ -1,40 +1,62 @@
-extern crate rayon;
-
-use self::rayon::prelude::*;
-
 use day10::KnotHasher;
 
-pub fn create_grid_for_input(input: &str) -> Vec<String>
-{
+pub fn create_grid_for_input(input: &str) -> Vec<Vec<i32>> {
     let mut hasher = KnotHasher::new();
-    
+
     (0..128)
-    .map(|a| {
-        let new_input: String = format!("{}-{}", input, a);
-        hasher.do_knot_for_input_second(new_input.as_str());
-        hash_to_binary(hasher.get_dense_hash())
-    })
-    .collect()
+        .map(|a| {
+            let new_input: String = format!("{}-{}", input, a);
+            hasher.do_knot_for_input_second(new_input.as_str());
+            hash_to_binary(hasher.get_dense_hash())
+        })
+        .map(|a| {
+            let mut line: Vec<i32> = Vec::new();
+            a.chars().for_each(|b| if b == '1' {
+                line.push(1);
+            } else {
+                line.push(0);
+            });
+            line
+        })
+        .collect()
 }
 
 pub fn hash_to_binary(input: String) -> String {
-    let mut global: String = String::from("");
     input
-    .chars()
-    .for_each(|a| {
-        let to_concat = format!("{:b}", a.to_digit(16).unwrap());
-        global = format!("{}{}", global, to_concat);
-    });
-
-    global
+        .chars()
+        .map(|a| format!("{:04b}", a.to_digit(16).unwrap()))
+        .collect()
 }
 
-pub fn count_used_cells(grid: &Vec<String>) -> i32
-{
-    grid
-    .par_iter()
-    .map(|a| {
-        a.chars().filter(|b| *b == '1').count() as i32
-    })
-    .sum()
+pub fn count_used_cells(grid: &Vec<Vec<i32>>) -> i32 {
+    grid.iter()
+        .map(|a| a.iter().filter(|b| *b == &1).count() as i32)
+        .sum()
+}
+
+pub fn count_groups(grid: &Vec<Vec<i32>>) -> i32 {
+    let mut nb_groups: i32 = 0;
+
+    for i in 0..128 {
+        for j in 0..128 {
+            if get_case(grid, i, j) == 1 {
+                if !(get_case(grid, i as i32 - 1, j as i32) == 1
+                    || get_case(grid, i as i32 + 1, j as i32) == 1
+                    || get_case(grid, i as i32, j as i32 - 1) == 1
+                    || get_case(grid, i as i32, j as i32 + 1) == 1)
+                {
+                    nb_groups += 1;
+                }
+            }
+        }
+    }
+
+    nb_groups
+}
+
+fn get_case(grid: &Vec<Vec<i32>>, x: i32, y: i32) -> i32 {
+    if x < 0 || x >= 128 || y < 0 || y >= 128 {
+        return 0;
+    }
+    grid[x as usize][y as usize]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate advent_of_code_2017;
 
-use advent_of_code_2017::day14::{create_grid_for_input, count_used_cells};
+use advent_of_code_2017::day14::{count_groups, count_used_cells, create_grid_for_input};
 
 fn main() {
-    
-    let input: &str = "jxqlasbh";
-    let grid: Vec<String> = create_grid_for_input(input);
-    println!("res: {}", count_used_cells(&grid));
+    //let input: &str = "jxqlasbh";
+    let input: &str = "flqrgnkx";
+    let grid: Vec<Vec<i32>> = create_grid_for_input(input);
 
+    //println!("res: {}", count_used_cells(&grid));
+    println!("res: {}", count_groups(&grid));
+    // should print 1242
 }
