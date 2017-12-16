74d0d0dd4667751821d6ded17a64a8022ece30b0
AlisCode <oliv.pinon@gmail.com>
2017-12-16 19:31:03 +0100

    Working on day7 - part 1 done

--- src/day7.rs
+++ src/day7.rs
@@ -1,17 +1,21 @@
 extern crate day7_parser;
-extern crate petgraph;
 
 use day7_parser::Process;
 
-use self::petgraph::Graph;
-
-pub fn generate_graph<'a>(list: &Vec<Process>) -> Graph<(), Process> {
-    let mut graph = Graph::new();
+pub fn get_root_process<'a>(list: &'a Vec<Process>) -> &'a str {
+    let mut children_processes: Vec<&String> = Vec::new();
 
     list.iter().for_each(|a| {
-        graph.add_node(&a);
+        a.children_names.iter().for_each(|b| {
+            children_processes.push(&b);
+        });
     });
 
-
-    graph
+    match list.iter()
+        .filter(|a| !children_processes.contains(&&a.name))
+        .next()
+    {
+        Some(res) => &res.name,
+        _ => "No result",
+    }
 }
